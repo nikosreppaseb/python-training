945a825c0a306508ae07abd81465f1b055411884
nikosreppaseb <reppas.nikolaos@nbg.gr>
2025-04-19 08:35:16 +0300

    exxample with ternary operator

--- meaning.py
+++ meaning.py
@@ -1,11 +1,11 @@
-meaning = 40
+life = 80
+geros = False
 
-if meaning > 20:
+if life > 20:
  print("The meaning of life is greater than 20")
 else:
  print("The meaning of life is less than or equal to 20")
 
-
-# The meaning of life is greater than 20
-# This code compares the variable `meaning` to 20 and prints a message based on the comparison.
-# The meaning of life is less than or equal to 20       
+geros = True if life > 80 else False
+# The meaning of life is 42
+print(geros)
