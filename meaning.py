306394c79fd4fbfc38ac5b8235bacdb30c15dbe7
nikosreppaseb <reppas.nikolaos@nbg.gr>
2025-04-19 08:32:23 +0300

    another fix on bug

--- meaning.py
+++ meaning.py
@@ -4,7 +4,8 @@ if meaning > 20:
  print("The meaning of life is greater than 20")
 else:
  print("The meaning of life is less than or equal to 20")
-    
+
+
 # The meaning of life is greater than 20
 # This code compares the variable `meaning` to 20 and prints a message based on the comparison.
 # The meaning of life is less than or equal to 20       
