3d9cb33946f946035860cd0af6ba77122ead8565
nikosreppaseb <reppas.nikolaos@nbg.gr>
2025-04-20 08:31:07 +0300

    User Input training .py

---
+++ user_input_training.py
@@ -0,0 +1,3 @@
+value = input("Enter a value: ")
+print(f"You entered: {value}")
+# The input function in Python 3.x returns a string, so if you want to get an integer or float, you need to convert it.
